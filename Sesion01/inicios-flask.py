4babe739f8ba888800103eb8114fdc85b577c5fd
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-12-16 22:06:18 -0500

    chore: avance flask

--- Sesion01/inicios-flask.py
+++ Sesion01/inicios-flask.py
@@ -5,4 +5,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.run()
+# el decorador sirve para usar el metodo de una clase pero implementadolo en una funcion
+@app.route('/')
+def inicio():
+    return 'Bienvenido a mi API'
+
+
+@app.route('/bienvenido')
+def bienvenido():
+    return 'Te doy la bienvenida a mi API'
+
+@app.route('/bienvenido/home')
+def bienvenido_home():
+    # solamente podemos retornar los siguientes tipos de datos: Strings, tupla y diccionarios
+    return 'adios'
+
+if __name__ == '__main__':
+    # debug >para que cada vez que nosotros hagamos algun cambio en cualquier archivo del proyecto y se reinicie automaticamente entonces deberemos indicar el parametro debug con el valor de True (cuyo valor por defecto es False)
+    # port> cambiara el puerto donde escuchara nuestro backend
+    app.run(debug=True, port=3000)
