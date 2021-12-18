f32c86e246c4fbdfd8ae678b1fbeef13f8695323
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-12-18 17:27:47 -0500

    chore: avance paso de parametros por la url

--- Sesion02/api-flask.py
+++ Sesion02/api-flask.py
@@ -1,7 +1,20 @@
 from flask import Flask, request
+from flask.helpers import make_response
 # el request nos da toda la informacion del cliente
 
 app = Flask(__name__)
+
+mis_productos = [{
+    "nombre":"Paneton con arto bromato",
+    "precio": 17.50,
+    "disponible": True,
+    "fecha_vencimiento": "2022-01-14"
+}, {
+    "nombre":"Chocolate con arta azucar",
+    "precio": 6.90,
+    "disponible": False,
+    "fecha_vencimiento": "2021-12-30"
+}]
 
 # url? no, la url es el dominio mas el endpoint => 127.0.0.1:5000/
 # endpoint
@@ -28,9 +41,53 @@ def inicio():
         # cuando retornamos una respuesta puede contener hasta dos parametros en el cual el primero sera la data enviada y el segundo sera el estado HTTP
         return 'Bienvenido a mi API de Productos', 200
 
-@app.route('/productos')
+@app.route('/productos', methods= ['GET', 'POST'])
 def productos():
-    pass
+    if request.method == 'GET':
+        return {
+            'data': mis_productos,
+            'message': 'Los productos son:',
+            'ok': True
+        }
+
+    elif request.method == 'POST':
+        data = request.get_json()
+        mis_productos.append(data) # insertando un registro en una bd (INSERT INTO ...)
+        return {
+            'data': data,
+            'message': 'Producto agregado exitosamente',
+            'ok': True
+        }, 201
+
+# al nosotros ponerle el tipo de dato que podemos aceptar y si al momento de enviar no es ese tipo de dato entonces se rechazara automaticamente la peticion con un estado 404 NOT FOUND
+@app.route('/producto/<int:id>', methods=['GET'])
+def producto(id):
+    if request.method == 'GET':
+        # Solucion: Sacar la longitud de la lista y validar si la posicion deseada es MENOR que la longitud si es MAYOR entonces no existe len(lista)
+
+        # recibir el ID que vendria a ser LA POSICION DE LA LISTA (recordemos que empieza en 0) y dar el producto a buscar, SI NO EXISTE el producto entonces emitir un message que diga que el producto no existe 
+        # {
+        #     'data': null,
+        #     'message': 'El producto no existe',
+        #     'ok': False
+        # }
+        # Si el producto si existe entonces dar el producto
+        if (id < len(mis_productos)):
+            return {
+                'ok': True,
+                'data': mis_productos[id],
+                'message':'El producto es:'
+            }
+        else:
+            return {
+                'ok': False,
+                'data': None,
+                'message': 'El producto no existe'
+            }
+        
+    
+
+
 
 
 if __name__ == '__main__':
