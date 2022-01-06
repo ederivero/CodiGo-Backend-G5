7b10b72acc338b9a46f1d588f0bae0415a65411f
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-01-05 20:22:25 -0500

    chore: ejercicio herencia

--- Sesion01/3-clases-con-herencia.py
+++ Sesion01/3-clases-con-herencia.py
@@ -17,20 +17,25 @@ class Vehiculo:
         self.__velocidad -= 20
         return self.__velocidad
     
+    def get_velocidad(self):
+        return self.__velocidad
+    
 
 class VehiculoVolador(Vehiculo):
     def __init__(self, color, modelo, traccion, vuela=False):
         super().__init__(color, modelo, traccion)
-        self.vuela = vuela
+        self.__vuela = vuela
     
     def volar(self):
-        self.vuela = True
+        self.__vuela = True
     
     def aterrizar(self):
-        self.vuela = False
+        self.__vuela = False
 
     def estado(self):
-        'el vehiculo es de color: .... modelo: .... traccion: ... velocidad: ... y esta volando / esta aterrizado'
+        estado_volando = 'esta volando' if self.__vuela else 'esta aterrizado'
+        return 'el vehiculo es de color: {} modelo: {} traccion: {} velocidad: {} y {}'.format(self.color, self.modelo, self.traccion, self.get_velocidad(), estado_volando)
+        
 class VehiculoOffRoad(VehiculoVolador):
     def __init__(self, color, modelo, traccion, vuela=False, sumergido=False):
         super().__init__(color,modelo,traccion, vuela)
@@ -38,8 +43,9 @@ class VehiculoOffRoad(VehiculoVolador):
 
 obj_vehiculo = Vehiculo('verde','rx5', '4x4')
 obj_vehiculo_volador = VehiculoVolador('blanco','xyz','4x2')
-obj_vehiculo_volador.volar()
-print(obj_vehiculo_volador.vuela) # debe lanzar un error que no existe
+# obj_vehiculo_volador.volar()
+obj_vehiculo_volador.acelerar()
+# print(obj_vehiculo_volador.vuela) # debe lanzar un error que no existe
 print(obj_vehiculo_volador.color)
 
 # print(obj_vehiculo.vuela)
@@ -50,3 +56,4 @@ obj_vehiculo_offroad.desacelerar()
 obj_vehiculo_offroad.volar()
 
 # Primero que el atributo vuela sea privado y luego tener un metodo llamado estado en el cual me indique cual es el estado del vehiculo, que me diga su color, modelo, traccion, velocidad y si esta volando o si esta aterrizado
+print(obj_vehiculo_volador.estado())
