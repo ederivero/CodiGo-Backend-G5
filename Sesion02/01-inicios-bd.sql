db6d24a47cb2dac515a5c95e265da0870ed4ebfb
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-01-06 20:25:32 -0500

    chore: avance DML

--- Sesion02/01-inicios-bd.sql
+++ Sesion02/01-inicios-bd.sql
@@ -4,11 +4,13 @@ USE pruebas;
 -- https://dev.mysql.com/doc/refman/8.0/en/data-types.html
 CREATE TABLE personas(
 	-- Ahora definimos las columnas pertenecientes a esta tabla
-    id INT, -- solamente se podran almacenar numeros
-    nombre VARCHAR(100), -- se podran almacenar caract. HASTA 100 como maximo
-    dni CHAR(8), -- siempre se almacenaran 8 caracteres
+    -- solamente se puede tener una sola pk (primary key) por tabla y ademas una sola columna
+    -- auto incrementable
+    id INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT, -- solamente se podran almacenar numeros
+    nombre VARCHAR(100) NOT NULL, -- se podran almacenar caract. HASTA 100 como maximo
+    dni CHAR(8) UNIQUE NOT NULL, -- siempre se almacenaran 8 caracteres
     fecha_nacimiento DATE, -- seran solamente fecha
-    created_at DATETIME, -- sera fecha y hora , minuto, segundo
+    created_at DATETIME NOT NULL, -- sera fecha y hora , minuto, segundo
     sexo ENUM('MASCULINO','FEMENINO','OTRO','HELICOPTERO'), -- solamente podra tener los valores definidos en el parentesis
     estado BOOL -- o sera true o false
 );
