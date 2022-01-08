75383c488ee0a32f52824bd31620466c2cf504fe
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-01-07 21:10:23 -0500

    chore: bd colegio

--- Sesion03/DB COLEGIO.sql
+++ Sesion03/DB COLEGIO.sql
@@ -3,8 +3,26 @@
 -- COMUNICACION, CTA, INGLES, FRENCH
 -- EL ALUMNO TIENE UN NOMBRE, APELLIDO, CORREO
 -- UN ALUMNO PUEDE LLEVAR VARIOS CURSOS Y UN CURSO PUEDE TENER VARIOS ALUMNOS
-
+-- es que las FK y las PK's no pueden ser nulas 
 CREATE DATABASE colegio;
-CREATE TABLE ALUMNOS(...);
-CREATE TABLE CURSOS(...);
-CREATE TABLE ALUMNOS_CURSOS(...);
+use colegio;
+
+CREATE TABLE ALUMNOS(
+	id int not null primary key auto_increment,
+    nombre varchar(100),
+    apellido varchar(100),
+    correo varchar(50)
+);
+CREATE TABLE CURSOS(
+	id int not null primary key auto_increment,
+    nombre varchar(100)
+);
+
+CREATE TABLE ALUMNOS_CURSOS(
+	id int not null primary key auto_increment,
+    curso_id int not null,
+    alumno_id int not null,
+    foreign key (curso_id) references cursos(id),
+    foreign key (alumno_id) references alumnos(id)
+);
+
