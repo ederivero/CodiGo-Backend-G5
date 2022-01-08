4ce42e6c18d3879c24f668a818305c80542a24c7
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-01-08 15:32:00 -0500

    chore: modificacion limite de alumnos

--- Sesion03/simulador.py
+++ Sesion03/simulador.py
@@ -54,7 +54,7 @@ data = [[1,3],[10,2],[32,1],[55,4],[86,3],[10,1]]
 x = 0
 while x < 200:
     curso = objFaker.random_int(min=1, max=4)
-    alumno = objFaker.random_int(min=1, max=100)
+    alumno = objFaker.random_int(min=1, max=99)
     if [alumno, curso] not in data:
         x+=1
         print(f'INSERT INTO ALUMNOS_CURSOS (ALUMNO_ID, CURSO_ID) VALUES({alumno}, {curso});')
