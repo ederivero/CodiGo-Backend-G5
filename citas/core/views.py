41812d11334046a00449d24409280d30d04aa765
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-01-15 19:07:21 -0500

    chore: avance DRF

--- citas/core/views.py
+++ citas/core/views.py
@@ -1,6 +1,11 @@
 from rest_framework.generics import CreateAPIView
+from .serializers import PersonasSerializer
+from rest_framework.response import Response
 
 class PersonasController(CreateAPIView):
-    def post(self):
+    # sera el encargado de moldear la informacion que nos llega a nuestro backend y a su vez de formatear la information para que pueda ser enviada al front
+    serializer_class=PersonasSerializer
+    def post(self, request):
+        print(request)
         """se ejecutara todo lo relacionado con el metodo post de este controlador"""
-        return 'Hola amigos'
+        return Response(data='Hola amigos')
