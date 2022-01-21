929de430cb684d4ed7db00f93f74e02b74f03573
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-01-20 19:50:40 -0500

    chore: avance express

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-import express from "express";
+import express, { json } from "express";
 // const express = require("express");
+const productos = [];
 
 const app = express();
+// middleware > es un intermediario entre todas las peticiones que se realicen a determinado endpoint o si no se indica a todas las peticiones de mi API
+// que mi application de express podra entender toda la informacion enviada por el cliente siempre y cuando sea un json
+app.use(json());
+
 const port = 3000;
 
 app.get("/", (req, res) => {
@@ -10,6 +15,22 @@ app.get("/", (req, res) => {
   res.status(200).json({
     message: "Peticion realizada exitosamente",
   });
+});
+
+app.post("/producto", (req, res) => {
+  console.log(req.body);
+
+  if (req.body) {
+    productos.push(req.body);
+    res.status(201).json({
+      message: "Producto agregado exitosamente",
+      producto: req.body,
+    });
+  } else {
+    res.status(400).json({
+      message: "Informacion incorrecta",
+    });
+  }
 });
 
 // se mantendra escuchando las consultas realizadas a este servidor mediante el puerto definido
