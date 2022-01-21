38f2c68d8e8a25778f99cf71d74411c4d89c4246
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-01-20 21:47:23 -0500

    chore: avance front

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,13 @@
 console.log("yo soy el front");
+const btnProductos = document.getElementById("btnProductos");
+
+async function traerProductos() {
+  const result = await fetch("http://localhost:3000/productos");
+  const data = await result.json();
+  console.log(data);
+}
+
+btnProductos.addEventListener("click", (e) => {
+  e.preventDefault();
+  traerProductos();
+});
