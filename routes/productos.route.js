1d8ac1a16bcb4ef0a773f18170868679cfe4eace
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-01-26 18:45:10 -0500

    chore: limpieza de la rama y nuevo proyecto

--- routes/productos.route.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { create, find } from "../controllers/productos.controller.js";
-
-export const productosRouter = Router();
-
-productosRouter.route("/producto").post(create);
-productosRouter.route("/productos").get(find);
