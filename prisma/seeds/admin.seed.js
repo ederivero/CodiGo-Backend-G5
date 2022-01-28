94762123c337c5591cfe6753b173297c47b32e57
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-01-27 19:21:58 -0500

    chore: finalizacion seed

--- prisma/seeds/admin.seed.js
+++ prisma/seeds/admin.seed.js
@@ -1,12 +1,18 @@
+import { hashSync } from "bcrypt";
+
 export default async (prisma) => {
+  const password = hashSync("Welcome123!", 10);
+
   await prisma.usuario.upsert({
     create: {
       nombre: "Eduardo",
       correo: "ederiveroman@gmail.com",
-      password: "123456",
+      password,
       tipoUsuario: "ADMIN",
     },
-    update: {},
+    update: {
+      password,
+    },
     where: {
       // solamente se pueden declarar las columnas que sean unicas (unique) o las pk
       correo: "ederiveroman@gmail.com",
