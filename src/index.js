269876bd478ccc2357ac69f22c49f341f89eb9cf
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-01-27 20:32:43 -0500

    chore: avance login

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import express, { json } from "express";
 import morgan from "morgan";
+import { authRouter } from "./routes/auth.routes.js";
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(json());
+
+// defino mis rutas
+app.use(authRouter);
+// fin de la definicion
 
 const PORT = process.env.PORT ?? 3000;
 
