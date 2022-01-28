87b7e1421d6a0705f7db4871be99781c76a0e0ee
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-01-27 22:03:34 -0500

    chore: finalizacion validacion

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,10 +3,15 @@ import { AuthService } from "../services/auth.service.js";
 import { loginDto } from "../services/dtos/request/login.dto.js";
 
 export async function login(req, res) {
-  const { correo, password } = req.body;
+  //   const { correo, password } = req.body;
   try {
-    loginDto({ correo, password });
+    // forma simplicada en la cual en un solo paso hacemos todo
+    // const result = await AuthService.login(loginDto(req.body));
 
+    // forma extendida en la cual primero llamo al dto para validar la informacion dada por el body y luegog eso me retornara lo que vendria a ser el correo y la passwor ya validados correctamente
+    const { correo, password } = loginDto(req.body);
+
+    // luego le pasaria esa informacion validada previamente al servicio para que ya se encarge de hacer la busqueda correspondiente del usuario
     const result = await AuthService.login({ correo, password });
 
     res.status(200).json(result);
