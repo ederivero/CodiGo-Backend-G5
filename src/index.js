7d13cd23d47d1a5e10fcdda85c7200058bc47674
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-01-27 19:59:03 -0500

    chore: avance morgan

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import express from "express";
+import express, { json } from "express";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(json());
+
 const PORT = process.env.PORT ?? 3000;
 
 app.listen(PORT, () => {
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,3 +1,14 @@
+import { prisma } from "../prisma.js";
+
 export class AuthService {
-  static async login() {}
+  static async login({ correo, password }) {
+    // SELECT password, tipo_usuario FROM USUARIO WHERE correo= '...';
+    // si no lo encuentra lanzara un error de not found
+    const usuarioEncontrado = await prisma.usuario.findUnique({
+      where: { correo },
+      select: { password: true, tipoUsuario: true },
+    });
+
+    return { message: "Si existe" };
+  }
 }
