5d3d753c866c7784b1e54892208e8ae4b9775a1b
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-01-28 19:05:00 -0500

    chore: avance jwt

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,4 +1,6 @@
 import { prisma } from "../prisma.js";
+import { compareSync } from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export class AuthService {
   static async login({ correo, password }) {
@@ -6,10 +8,23 @@ export class AuthService {
     // si no lo encuentra lanzara un error de not found
     const usuarioEncontrado = await prisma.usuario.findUnique({
       where: { correo },
-      select: { password: true, tipoUsuario: true },
+      select: { password: true, tipoUsuario: true, id: true },
       rejectOnNotFound: true,
     });
 
-    return { message: "Si existe" };
+    const resultado = compareSync(password, usuarioEncontrado.password);
+
+    if (resultado) {
+      const token = jwt.sign(
+        { id: usuarioEncontrado.id, mensaje_oculto: "Hola soy un mensaje" },
+        "ArribaPeru",
+        // se puede pasar un valor numerico (que sera en segundos) o un string indicando el formato de la sgte manera '7d' (7 dias) '10h' (10 horas), '2 days' (2 dias), si le ponemos '100' entonces sera un valor expresado en milisegundos
+        { expiresIn: 100 }
+      );
+
+      return { message: "Si es el usuario", token };
+    } else {
+      return { message: "Credenciales incorrectas" };
+    }
   }
 }
