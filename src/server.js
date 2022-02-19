f55fcec748ba569d81575f8e63622c8af8c58c29
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-02-18 21:52:37 -0500

    chore: avance mp

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@ import mongoose from "mongoose";
 import { categoriaRouter } from "./routes/categoria.routes.js";
 import { categoriaProductoRouter } from "./routes/categoria_producto.routes.js";
 import { productoRouter } from "./routes/producto.routes.js";
+import mercadopago from "mercadopago";
+
+// access_token > es la token que se va a crear por cada negocio y esto servira a MP para saber a que negocio tiene que depositar el monto cobrado
+// integrator_id > el id del desarrollador que esta haciendo esta integracion
+mercadopago.configure({
+  access_token: process.env.ACCESS_TOKEN_MP,
+  integrator_id: process.env.INTEGRATOR_ID_MP,
+});
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
